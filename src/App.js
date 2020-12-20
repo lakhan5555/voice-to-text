a3da08abd89a292e851ce0714626ca3304d5830c
Ram Lakhan <ramlakhansingh5555@gmail.com>
2020-12-21 01:32:00 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+mic.continous = true
+mic.interimResults = true
+mic.lang = "en-US"
+
+
 function App() {
+
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const [savedNotes, setSavedNotes] = useState([])
+
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if(isListening){
+      mic.start()
+      mic.onend = () => {
+        console.log("Continue..");
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log("Stopped Mic on Click");
+      }
+    }
+
+    mic.onstart = () => {
+      console.log("Mic on");
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+
+      console.log(transcript);
+
+      setNote(transcript)
+
+      mic.onerror = event => {
+        console.log(event.error);
+      }
+    }
+
+  }
+
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Voice Notes</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Current Note</h2>
+          <button onClick={handleSaveNote} disabled={!note}>Save Note</button>
+          <button onClick={() => setIsListening(prevSatate => !prevSatate)}>Start/Stop</button>
+          <p>{note}</p>
+        </div> 
+        <div className="box">
+          <h2>Notes</h2>
+          {savedNotes.map(n => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
